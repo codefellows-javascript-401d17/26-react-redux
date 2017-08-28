3a493beba9ca65c9c235f2da3eb0d69357800c1a
Luis Manzo <manzosk3@gmail.com>
2017-08-28 16:44:02 -0700

    sets up reducer

---
+++ src/reducer/category.js
@@ -0,0 +1,22 @@
+
+let initialState = [];
+
+export default =  (state=initialState, action) => {
+  let { type, payload } = action;
+  switch (type) {
+    case 'CATEGORY_CREATE':
+      return ([...state, payload]);
+    case 'CATEGORY_UPDATE':
+      return (state.map((category) => {
+        return category.id === payload.id ? payload : category;
+      }))
+    case 'CATEGORY_DELETE':
+      return state.filter((category) => {
+        return category.id === payload.id;
+      })
+    case 'CATEGORY_RESET':
+      return state = initialState;
+    default:
+      return state;
+  }
+}
