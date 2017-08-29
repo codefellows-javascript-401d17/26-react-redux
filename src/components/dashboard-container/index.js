6768f03a3965e017d8dcdc9d94e185c73dbc80b3
Luis Manzo <manzosk3@gmail.com>
2017-08-28 19:10:37 -0700

    dynamically logs input values to console update and submit

--- src/components/dashboard-container/index.js
+++ src/components/dashboard-container/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import {
+  categoryCreate,
+  categoryUpdate,
+  categoryDelete
+} from '../../action/category-actions.js';
 
 class DashboardContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.category ? props.category.name : '',
+      budget: props.category ? props.category.budget : 0
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  componentDidMount() {
+    console.log('state in dashboardcontainer', this.state)
+    console.log('props in dashboardcontainer', this.props);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log('event target is', e.target);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    
+  }
+
+  // componentDidMount() {
+  //   categoryCreate({ name: 'rent', budget: 800 });
+  //   categoryCreate({ name: 'pizza', budget: 500 });
+  // }
 
   render() {
-
-
     return (
       <section>
         <h2>Dashboard</h2>
@@ -14,11 +47,14 @@ class DashboardContainer extends React.Component {
             type='text'
             name='name'
             onChange={this.handleChange}
+            value={this.state.name}
             placeholder='enter a category name'
           />
           <input
+            id='budgetField'
             type='text'
             name='budget'
+            value={this.state.budget}
             onChange={this.handleChange}
             placeholder='enter a budget for this category'
           />
